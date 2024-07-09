ed5a354394a5770b74e505e011174fccfd8759f5
gorkemksz <gorkemoksuz20@gmail.com>
2024-07-10 01:08:07 +0300

    Student registration app

---
+++ Student registration app/Student registration app/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace Student_registration_app
+{
+    partial class StudentInformations
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbSurname = new System.Windows.Forms.TextBox();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbStudentId = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.DgStudents = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbPlaceBirth = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbGender = new System.Windows.Forms.ComboBox();
+            this.dtpDateBirth = new System.Windows.Forms.DateTimePicker();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbMail = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbPhoneNumber = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DgStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(109, 87);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(121, 20);
+            this.tbName.TabIndex = 0;
+            this.tbName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbName_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Surname";
+            // 
+            // tbSurname
+            // 
+            this.tbSurname.Location = new System.Drawing.Point(109, 122);
+            this.tbSurname.Name = "tbSurname";
+            this.tbSurname.Size = new System.Drawing.Size(121, 20);
+            this.tbSurname.TabIndex = 3;
+            this.tbSurname.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbSurname_KeyPress);
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(109, 51);
+            this.tbID.MaxLength = 5;
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(121, 20);
+            this.tbID.TabIndex = 5;
+            this.tbID.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbID_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Student ID";
+            // 
+            // tbStudentId
+            // 
+            this.tbStudentId.Location = new System.Drawing.Point(109, 158);
+            this.tbStudentId.MaxLength = 5;
+            this.tbStudentId.Name = "tbStudentId";
+            this.tbStudentId.Size = new System.Drawing.Size(121, 20);
+            this.tbStudentId.TabIndex = 7;
+            this.tbStudentId.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbStudentId_KeyPress);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(97, 419);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add Student";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // DgStudents
+            // 
+            this.DgStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgStudents.Location = new System.Drawing.Point(397, 87);
+            this.DgStudents.Name = "DgStudents";
+            this.DgStudents.Size = new System.Drawing.Size(861, 509);
+            this.DgStudents.TabIndex = 9;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(85, 459);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 23);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete Student";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(85, 499);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 23);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Update Student";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Gender";
+            // 
+            // tbPlaceBirth
+            // 
+            this.tbPlaceBirth.Location = new System.Drawing.Point(109, 308);
+            this.tbPlaceBirth.MaxLength = 5;
+            this.tbPlaceBirth.Name = "tbPlaceBirth";
+            this.tbPlaceBirth.Size = new System.Drawing.Size(121, 20);
+            this.tbPlaceBirth.TabIndex = 15;
+            this.tbPlaceBirth.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbPlaceBirth_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 315);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Place of birth";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(29, 351);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Date of birth";
+            // 
+            // cmbGender
+            // 
+            this.cmbGender.FormattingEnabled = true;
+            this.cmbGender.Items.AddRange(new object[] {
+            "Girl ",
+            "Male"});
+            this.cmbGender.Location = new System.Drawing.Point(109, 194);
+            this.cmbGender.Name = "cmbGender";
+            this.cmbGender.Size = new System.Drawing.Size(121, 21);
+            this.cmbGender.TabIndex = 18;
+            // 
+            // dtpDateBirth
+            // 
+            this.dtpDateBirth.Location = new System.Drawing.Point(109, 345);
+            this.dtpDateBirth.Name = "dtpDateBirth";
+            this.dtpDateBirth.Size = new System.Drawing.Size(200, 20);
+            this.dtpDateBirth.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(29, 235);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "E -- mail";
+            // 
+            // tbMail
+            // 
+            this.tbMail.Location = new System.Drawing.Point(109, 232);
+            this.tbMail.Name = "tbMail";
+            this.tbMail.Size = new System.Drawing.Size(121, 20);
+            this.tbMail.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(27, 272);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(76, 13);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "Phone number";
+            // 
+            // tbPhoneNumber
+            // 
+            this.tbPhoneNumber.Location = new System.Drawing.Point(109, 269);
+            this.tbPhoneNumber.MaxLength = 11;
+            this.tbPhoneNumber.Name = "tbPhoneNumber";
+            this.tbPhoneNumber.Size = new System.Drawing.Size(121, 20);
+            this.tbPhoneNumber.TabIndex = 22;
+            this.tbPhoneNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbPhoneNumber_KeyPress);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(437, 28);
+            this.txtSearch.MaxLength = 10;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(423, 20);
+            this.txtSearch.TabIndex = 24;
+            this.txtSearch.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSearch_KeyPress);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(323, 35);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(108, 13);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "Search for student ID";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(875, 26);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 26;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // StudentInformations
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1351, 658);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.tbPhoneNumber);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tbMail);
+            this.Controls.Add(this.dtpDateBirth);
+            this.Controls.Add(this.cmbGender);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbPlaceBirth);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.DgStudents);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbStudentId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.tbSurname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbName);
+            this.Name = "StudentInformations";
+            this.Text = "StudentInformations";
+            this.Load += new System.EventHandler(this.StudentInformations_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbSurname;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbStudentId;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView DgStudents;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbPlaceBirth;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cmbGender;
+        private System.Windows.Forms.DateTimePicker dtpDateBirth;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbMail;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tbPhoneNumber;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ Student registration app/Student registration app/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_registration_app
+{
+    public partial class StudentInformations : Form
+    {
+        SqlConnection con = new SqlConnection(@"Database Path");
+        public StudentInformations()
+        {
+            InitializeComponent();
+        }
+        private void tbName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void tbSurname_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        private void tbPlaceBirth_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void tbStudentId_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void tbPhoneNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void tbID_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        void StudentInfo()
+        {
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter("Select * from Students", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            DgStudents.DataSource = dt;
+            con.Close();
+        }
+        private void StudentInformations_Load(object sender, EventArgs e)
+        {
+            StudentInfo();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand ("insert into [Students] (ID,_Name,Surname,StudentID,Gender,Email,PhoneNumber,PlaceOfBirth,DateOfBirth) values('" + tbID.Text + "','" + tbName.Text + "','" + tbSurname.Text + "','" + tbStudentId.Text + "','" + cmbGender.Text + "','" + tbMail.Text + "','" + tbPhoneNumber.Text + "','" + tbPlaceBirth.Text + "','" + dtpDateBirth.Value.ToString("yyyy/MM/dd")+ "')",con);
+            cmd.CommandType = CommandType.Text;
+            cmd.ExecuteNonQuery();
+            con.Close();
+            StudentInfo();
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from [Students] where ID = @ID ",con);
+            cmd.Parameters.AddWithValue("@ID",int.Parse(txtSearch.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            StudentInfo();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Update [Students] set _Name=@_Name, Surname=@Surname, StudentID=@StudentID, Gender=@Gender, Email=@Email, PhoneNumber=@PhoneNumber, PlaceOfBirth=@PlaceOfBirth, DateOfBirth=@DateOfBirth where ID = @ID", con);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(tbID.Text));
+            cmd.Parameters.AddWithValue("@_Name",(tbName.Text));
+            cmd.Parameters.AddWithValue("@Surname", (tbSurname.Text));
+            cmd.Parameters.AddWithValue("@StudentID",int.Parse(tbStudentId.Text));
+            cmd.Parameters.AddWithValue("@Gender",(cmbGender.Text));
+            cmd.Parameters.AddWithValue("@Email",(tbMail.Text));
+            cmd.Parameters.AddWithValue("@PhoneNumber", int.Parse(tbPhoneNumber.Text));
+            cmd.Parameters.AddWithValue("@PlaceOfBirth", (tbPlaceBirth.Text));
+            cmd.Parameters.AddWithValue("@DateOfBirth",dtpDateBirth.Value.ToString("yyyy/MM/dd"));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            StudentInfo();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from [Students] where ID like '%" + txtSearch.Text + "%'",con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da .Fill(dt);
+            DgStudents.DataSource = dt;
+            con.Close();
+        }
+    }
+}
